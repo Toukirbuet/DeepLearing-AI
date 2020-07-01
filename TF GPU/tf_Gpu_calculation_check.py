21491540e7fbf5a045f63e0451beaaea7aef9443
ToukirBuet <toukirahmedreal@gmail.com>
2020-07-01 16:59:11 +0600

    new certificate added introduction to tf https://coursera.org/share/154543d5d286e198174b2f6d27f46f9c

--- TF GPU/tf_Gpu_calculation_check.py
+++ TF GPU/tf_Gpu_calculation_check.py
@@ -1,11 +1,35 @@
 import tensorflow as tf
+import timeit
 
-tf.config.experimental.list_physical_devices('GPU') 
-tf.debugging.set_log_device_placement(True)
+device_name = tf.test.gpu_device_name()
+
+# tf.config.experimental.list_physical_devices('GPU') 
+# tf.debugging.set_log_device_placement(True)
 
 # Create some tensors
-a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-c = tf.matmul(a, b)
 
-print(c)
+def cpu():
+  with tf.device('/cpu:0'):
+      tf.random.set_seed(5)
+      a = tf.random.normal([2000,2000], 0, 1, tf.float32, seed=1)
+      b = tf.random.normal([2000,2000], 0, 1, tf.float32, seed=1)
+      c = tf.matmul(a, b)
+      return c
+def gpu():
+  with tf.device('/device:GPU:0'):
+      tf.random.set_seed(5)
+      a = tf.random.normal([2000,2000], 0, 1, tf.float32, seed=1)
+      b = tf.random.normal([2000,2000], 0, 1, tf.float32, seed=1)
+      c = tf.matmul(a, b)
+      return c
+
+
+print('CPU (s):')
+print(cpu())
+cpu_time = timeit.timeit('cpu()', number=1, setup="from __main__ import cpu")
+print(cpu_time)
+print('GPU (s):')
+print(gpu())
+gpu_time = timeit.timeit('gpu()', number=1, setup="from __main__ import gpu")
+print(gpu_time)
+print('GPU speedup over CPU: {}x'.format(int(cpu_time/gpu_time)))
